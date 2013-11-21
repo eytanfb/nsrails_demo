8dd3422386a9342d40c2d6c761b07a7d077fc7f7
Eytan Anjel <anjel@usc.edu>
2013-11-21 02:03:10 -0800

    initial commit for blog post

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NsrailsDemo::Application.config.secret_token = '37497a4917199e5a48f7bef5a8919d5a141b026d1910adf884101e6079e46508a089f4a087e9f8089291b2a003e946d6377b5cfef7417617f3ebc2c11b7419fb'
